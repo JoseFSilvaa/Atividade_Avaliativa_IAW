4073d2af358708561e7c930e686732896160ca73
José Ferreira <jose.silva.42@academico.ifpb.edu.br>
2025-08-17 16:38:06 -0300

    atualização

--- Fundamentos/q03.js
+++ Fundamentos/q03.js
@@ -1,3 +1,7 @@
 let grauCelsius = 37;
-let grauFahrenheit = (grauCelsius * 9/5) + 32;
-console.log(`A temperatura em Fahrenheit é: ${grauFahrenheit}`);
+let grauFahrenheit = 98.7;
+
+let grauPFahrenheit = (grauCelsius * 9/5) + 32;
+let grauPCelsius = (grauFahrenheit - 32) * 5/9;
+console.log(`A temperatura Celsius -> ${grauCelsius} em Fahrenheit é: ${grauPFahrenheit}`);
+console.log(`A temperatura Fahrenheit -> ${grauFahrenheit} em Celsius é: ${grauPCelsius}`);
